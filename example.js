0f583903ee1336a8f59b01e8cc3773a9003c9316
alfred sang <i5ting@126.com>
2014-10-23 14:12:19 +0800

    add callback for export api

--- example.js
+++ example.js
@@ -4,4 +4,10 @@ var src    = 'test.png'
 var dest   = 'example_out.png'
 var degree = 1
 
-Rotate.rotate(src,dest,degree);
+Rotate.rotate(src,dest,degree,function(src,dest){
+	//when succ
+	console.log('ROTATE SUCCESS CALLBACK');	
+},function(src,dest){
+	//when fail
+	console.log('ROTATE FAIL CALLBACK');	
+});
